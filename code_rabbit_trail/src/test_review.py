1a3123f33e8e88adf07502d5369b69ae4cd0624d
Narra Bhanu Surendra <bhanusurendra.narra@nxp.com>
2025-08-21 16:04:52 +0530

    Added

--- code_rabbit_trail/src/test_review.py
+++ code_rabbit_trail/src/test_review.py
@@ -12,5 +12,13 @@ class myclass:
         self.Value=value
     def getvalue(self):return self.Value
 
+def subtract(b,a):
+    result =b-a
+    return result
+print(  subtract(9,5))
+
+
+def invokeNextActivity():
+    print("cannot ivoke depends on caller.")
 
 def display(): print("This is a very long message that should ideally be wrapped across multiple lines to maintain readability and follow PEP-8 guidelines.")
