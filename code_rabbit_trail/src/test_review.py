3d7560ef865d20b219c2f49466e1523be9206545
Narra Bhanu Surendra <bhanusurendra.narra@nxp.com>
2025-08-21 15:57:38 +0530

    Added test snippets

---
+++ code_rabbit_trail/src/test_review.py
@@ -0,0 +1,16 @@
+
+
+
+def add(a,b):
+  result=a+b
+  return  result
+print(  add(5 ,6))
+
+
+class myclass:
+    def __init__(self,value):
+        self.Value=value
+    def getvalue(self):return self.Value
+
+
+def display(): print("This is a very long message that should ideally be wrapped across multiple lines to maintain readability and follow PEP-8 guidelines.")
